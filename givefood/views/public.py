e6c5d17dc70b69c94b79fb934f922093d7d4768d
Jason Cartwright <jason@potatolondon.com>
2019-07-08 16:12:10 +0100

    cache homepage

--- givefood/views/public.py
+++ givefood/views/public.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(60*5)
 def public_index(request):
 
     template_vars = {}
