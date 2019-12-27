504714aa338efc6f01844643ae2985c3ad5ec8d1
Jason Cartwright <jason@potatolondon.com>
2019-12-27 15:48:16 +0700

    tweak wfbn layout

--- static/js/wfbn.js
+++ static/js/wfbn.js
@@ -32,10 +32,10 @@ function api_response() {
   table_html = "<table class='table needs'>";
   for (i in this.response) {
     table_html += "<tr>";
-    table_html += "<td><a href='" + this.response[i].shopping_list_url + "'>" + this.response[i].name + "</a><br><span class='distance'>" + this.response[i].distance_mi + "mi</span><br><span class='updated'>" + this.response[i].updated_text + " ago</span></td>";
+    table_html += "<td><a href='" + this.response[i].shopping_list_url + "'>" + this.response[i].name + "</a><br><span class='distance'>" + this.response[i].distance_mi + "mi away</span></td>";
     table_html += "<td class='need_detail'>";
     if (this.response[i].number_needs > 0) {
-      table_html += "<details><summary>" + this.response[i].number_needs + " items</summary>" + this.response[i].needs.replace(/\n/g, '<br>') + "</summary>";
+      table_html += "<details><summary>" + this.response[i].number_needs + " items</summary>" + this.response[i].needs.replace(/\n/g, '<br>') + "</summary></details><div class='updated'>Updated " + this.response[i].updated_text + " ago</div>";
     } else {
       table_html += "Nothing right now, thanks"
     }
