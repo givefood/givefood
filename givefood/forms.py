082776eca3b890746bb2a2380c3687879f30e759
Jason Cartwright <jason@potatolondon.com>
2019-12-31 12:36:14 +0700

    better need dropdown on order form

--- givefood/forms.py
+++ givefood/forms.py
@@ -11,6 +11,7 @@ class FoodbankForm(ModelForm):
 
 class OrderForm(ModelForm):
     foodbank = ModelChoiceField(queryset=Foodbank.objects.filter(is_closed = False).order_by('name'))
+    need = ModelChoiceField(queryset=FoodbankChange.objects.all().order_by('-created'))
     class Meta:
         model = Order
         fields = "__all__"
