e932de365af7931a77cb60f409b54c662c9f9c64
Jason Cartwright <jason@potatolondon.com>
2019-12-31 21:08:25 +0700

    don't need that

--- static/js/reading.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(window).on("swipeleft", function(e){
-    window.location = $("link[rel='next']").attr("href")
-  })
-  $(window).on("swiperight", function(e){
-    window.location = $("link[rel='prev']").attr("href")
-  })
-})
