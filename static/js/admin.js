04f5685cf14a74124208af9110d0d1917c48febf
Jason Cartwright <jason@potatolondon.com>
2020-01-22 17:31:07 +0000

    titlecase button on changetext form

--- static/js/admin.js
+++ static/js/admin.js
@@ -2,14 +2,25 @@ function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+function titleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
 const lattlong_field = document.querySelector("#id_latt_long");
-const address_field = document.querySelector("#id_address")
-const postcode_field = document.querySelector("#id_postcode")
+const address_field = document.querySelector("#id_address");
+const postcode_field = document.querySelector("#id_postcode");
+const change_text_field = document.querySelector("#id_change_text");
 
 const geolocation_url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCgc052pX0gMcxOF1PKexrTGTu8qQIIuRk&address="
 const map_url = "https://maps.googleapis.com/maps/api/staticmap?zoom=15&size=300x300&key=AIzaSyCgc052pX0gMcxOF1PKexrTGTu8qQIIuRk&scale=2&center="
 
 
+// LATTLONG
 if (lattlong_field) {
   var get_lattlong_btn = document.createElement('div');
   get_lattlong_btn.innerHTML = "<a href='#' id='get_lattlong_btn' class='extra-form-button button is-info'>Get LattLong</a>";
@@ -33,4 +44,17 @@ if (lattlong_field) {
     gl_req.send();
     event.preventDefault();
   })
+}
+
+
+// CHANGETEXT
+if (change_text_field) {
+  var titlecase_btn = document.createElement('div');
+  titlecase_btn.innerHTML = "<a href='#' id='titlecase_btn' class='extra-form-button button is-info'>Make Titlecase</a>";
+  insertAfter(titlecase_btn, change_text_field);
+  titlecase_btn = document.querySelector("#titlecase_btn");
+  titlecase_btn.addEventListener("click", function(event) {
+    change_text_field.value = titleCase(change_text_field.value);
+    event.preventDefault();
+  });
 }
