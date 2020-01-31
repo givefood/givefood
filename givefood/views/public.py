e08c58f29cc9210f54510df3b5ae923a3f901027
Jason Cartwright <jason@potatolondon.com>
2020-01-31 10:16:18 +0000

    wfbn version

--- givefood/views/public.py
+++ givefood/views/public.py
@@ -145,7 +145,7 @@ def public_gen_annual_report(request, year):
 @cache_page(60*2)
 def public_what_food_banks_need(request):
 
-    version = "59f66682"
+    version = "a2c600f4"
 
     foodbanks = Foodbank.objects.all()
     no_foodbanks = len(foodbanks)
