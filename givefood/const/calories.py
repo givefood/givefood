4fd86f4aad2657af956a18030c6b5f8270e79205
Jason Cartwright <jason@potatolondon.com>
2020-03-01 13:17:27 +0000

    calories

--- givefood/const/calories.py
+++ givefood/const/calories.py
@@ -416,4 +416,5 @@ CALORIES = {
     "Tesco British Semi Skimmed Uht Milk 6 X 1 Litre":50,
     "Tesco Peeled Plum Tomatoes 400G":25,
     "Jack's Peach Slices In Light Syrup 410G":60,
+    "Tesco Vegetable Soup 400G":50,
 }
