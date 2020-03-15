6a91d197824147a352d29dab33e2d0be56494a3c
Jason Cartwright <jason@potatolondon.com>
2020-03-15 17:54:32 +0000

    handle not knowing what a foodbank needs

--- static/js/wfbn.js
+++ static/js/wfbn.js
@@ -10,6 +10,7 @@ const requesting_loc_html = "<img src='/static/img/loading.gif' alt='Loading'> R
 const no_loc_apology_text = "Sorry, we tried to get your location automatically but couldn't. Try a postcode or address.";
 const no_addr_text = "Did you forget to enter an address?";
 const nothing_needed_text = "Nothing right now, thanks";
+const need_unknown_text = "Sorry. We don't know what's needed here, contact the food bank";
 
 function init() {
   var defaultBounds = new google.maps.LatLngBounds(
@@ -98,9 +99,11 @@ function api_response() {
     currentrow.querySelector("a").href = "/needs/click/" + slug + "/";
     currentrow.querySelector("a").textContent = name;
     currentrow.querySelector(".distance span").textContent = distance;
-    if (number_needs > 0 && needs != "Nothing") {
+    if (number_needs > 0 && needs != "Nothing" && needs != "Unknown") {
       if (number_needs > 1) {item_text = "items"} else {item_text = "item"};
       currentrow.querySelector(".fb_needs p").innerHTML = needs_html;
+    } else if (needs == "Unknown") {
+      currentrow.querySelector(".fb_needs").innerHTML = need_unknown_text;
     } else {
       currentrow.querySelector(".fb_needs").innerHTML = nothing_needed_text;
     }
