d95496e519f41c412d162afcec1c40c8a869ee45
Jason Cartwright <jason@potatolondon.com>
2020-04-10 23:08:12 +0100

    pull bulma lib from jsdelivr

--- givefood/settings.py
+++ givefood/settings.py
@@ -129,7 +129,7 @@ PREPEND_WWW = False
 CSP_DEFAULT_SRC = ("'self'", "*.gstatic.com", "syndication.twitter.com")
 # Inline styles are unsafe, but Django error pages use them. We later remove
 # `unsafe-inline` in settings_live.py
-CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "fonts.googleapis.com", "*.gstatic.com", "cdnjs.cloudflare.com", "platform.twitter.com", "ton.twimg.com")
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "fonts.googleapis.com", "*.gstatic.com", "cdnjs.cloudflare.com", "platform.twitter.com", "ton.twimg.com", "cdn.jsdelivr.net")
 CSP_FONT_SRC = ("'self'", "themes.googleusercontent.com", "*.gstatic.com", "maps.googleapis.com")
 CSP_FRAME_SRC = ("'self'", "www.google.com", "www.youtube.com", "accounts.google.com", "apis.google.com", "plus.google.com", "staticxx.facebook.com","www.facebook.com", "platform.twitter.com", "syndication.twitter.com", "web.facebook.com")
 CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", "'unsafe-eval'", "instant.page", "*.googleanalytics.com", "*.google-analytics.com", "ajax.googleapis.com", "cdnjs.cloudflare.com", "www.gstatic.com", "www.googletagmanager.com", "maps.googleapis.com", "connect.facebook.net","platform.twitter.com", "cdn.syndication.twimg.com", "syndication.twitter.com")
