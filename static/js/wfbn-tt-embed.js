6996cd2cb5764199b526a6df2410c792eaa546f0
Jason Cartwright <jason@potatolondon.com>
2020-05-06 19:29:51 +0100

    increase default height of embed for location dropdown

--- static/js/wfbn-tt-embed.js
+++ static/js/wfbn-tt-embed.js
@@ -1,4 +1,4 @@
 const gf_wfbn = document.querySelector("#gf-needs");
-const gf_iframe_html = "<iframe id='gf_headless_iframe' src='https://www.givefood.org.uk/needs/?headless=true&from=trusselltrust' style='width:100%;border:0;height:82px;' allow='geolocation'></iframe>"
+const gf_iframe_html = "<iframe id='gf_headless_iframe' src='https://www.givefood.org.uk/needs/?headless=true&from=trusselltrust' style='width:100%;border:0;height:150px;' allow='geolocation'></iframe>"
 gf_wfbn.innerHTML = gf_iframe_html
 document.getElementById('gf_headless_iframe').onload = function() {iFrameResize({ log: false }, '#gf_headless_iframe')}
