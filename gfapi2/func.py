726b0cb4b22218de616eb65bbe6452487a6d8bbf
Jason Cartwright <jason@potatolondon.com>
2020-09-01 19:37:24 +0100

    fix dicttoxml logging

--- gfapi2/func.py
+++ gfapi2/func.py
@@ -1,5 +1,6 @@
 import yaml
 import dicttoxml
+import logging
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 
 
@@ -14,7 +15,7 @@ def ApiResponse(data, obj_name, format):
     if format == "json":
         return JsonResponse(data, safe=False)
     elif format == "xml":
-        dicttoxml.set_debug(False)
+        dicttoxml.LOG.setLevel(logging.ERROR)
         xml_str = dicttoxml.dicttoxml(data, attr_type=False, custom_root=obj_name, item_func=xml_item_name)
         return HttpResponse(xml_str, content_type="text/xml")
     elif format == "yaml":
