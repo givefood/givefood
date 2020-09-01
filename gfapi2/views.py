b0cdd4c284dcaad233247b87d1434c01c1bec949
Jason Cartwright <jason@potatolondon.com>
2020-09-01 08:32:55 +0100

    api2 foodbanks

--- gfapi2/views.py
+++ gfapi2/views.py
@@ -2,12 +2,53 @@ from django.shortcuts import get_object_or_404
 
 from givefood.models import Foodbank
 from .func import ApiResponse
+from givefood.func import get_all_foodbanks
 
 DEFAULT_FORMAT = "json"
 
 
 def foodbanks(request):
-    pass
+
+    format = request.GET.get("format", DEFAULT_FORMAT)
+
+    foodbanks = get_all_foodbanks()
+    response_list = []
+
+    for foodbank in foodbanks:
+        response_list.append({
+            "name":foodbank.name,
+            "alt_name":foodbank.alt_name,
+            "slug":foodbank.slug,
+            "phone":foodbank.phone_number,
+            "email":foodbank.contact_email,
+            "address":foodbank.full_address(),
+            "postcode":foodbank.postcode,
+            "closed":foodbank.is_closed,
+            "lat_lng":foodbank.latt_long,
+            "network":foodbank.network,
+            "modified":foodbank.modified,
+            "urls": {
+                "self":"https://www.givefood.org.uk/api/2/foodbank/%s/" % (foodbank.slug),
+                "html":"https://www.givefood.org.uk/needs/at/%s/" % (foodbank.slug),
+            },
+            "charity": {
+                "registration_id":foodbank.charity_number,
+                "register_url":foodbank.charity_register_url(),
+            },
+            "politics": {
+                "parliamentary_constituency":foodbank.parliamentary_constituency,
+                "mp":foodbank.mp,
+                "mp_party":foodbank.mp_party,
+                "ward":foodbank.ward,
+                "district":foodbank.district,
+                "urls": {
+                    "self":"https://www.givefood.org.uk/api/2/constituency/%s/" % (foodbank.parliamentary_constituency_slug),
+                    "html":"https://www.givefood.org.uk/needs/in/constituency/%s/" % (foodbank.parliamentary_constituency_slug),
+                },
+            }
+        })
+
+    return ApiResponse(response_list, "foodbanks", format)
 
 
 def foodbank(request, slug):
