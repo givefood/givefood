b6dc8fccdf389afd337b9f823fa0421eceeef854
Jason Cartwright <jason@givefood.org.uk>
2020-11-30 11:28:35 +0000

    cleanup ApiResponse and add Access-Control-Allow-Origin

--- gfapi2/func.py
+++ gfapi2/func.py
@@ -30,22 +30,19 @@ def ApiResponse(data, obj_name, format):
         return HttpResponseBadRequest()
 
     if format == "json" or format == "geojson":
-        return JsonResponse(data, safe=False, json_dumps_params={'indent': 2})
+        response = JsonResponse(data, safe=False, json_dumps_params={'indent': 2})
     elif format == "xml":
         dicttoxml.LOG.setLevel(logging.ERROR)
         xml_str = dicttoxml.dicttoxml(data, attr_type=False, custom_root=obj_name, item_func=xml_item_name)
         xml_dom = parseString(xml_str)
         xml_str = xml_dom.toprettyxml()
-        return HttpResponse(xml_str, content_type="text/xml")
+        response = HttpResponse(xml_str, content_type="text/xml")
     elif format == "yaml":
         yaml_str = yaml.safe_dump(data, encoding='utf-8', allow_unicode=True, default_flow_style=False)
-        return HttpResponse(yaml_str, content_type="text/yaml")
-    # elif format == "geojson":
-    #     if obj_name == "foodbanks":
-    #         geojson_str = foodbank_geojson(data)
-    #     if obj_name == "constituency":
-    #         geojson_str = constituency_geojson(data)
-    #     return JsonResponse(geojson_str, safe=False, json_dumps_params={'indent': 2})
+        response = HttpResponse(yaml_str, content_type="text/yaml")
+    
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
 
 
 xml_item_name = lambda x: x[:-1]
