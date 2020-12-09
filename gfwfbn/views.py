e847e12aa3a1d7cabd3c049dc65ff9badfb487c2
Jason Cartwright <jason@givefood.org.uk>
2020-12-09 19:13:54 +0000

    fix missing import

--- gfwfbn/views.py
+++ gfwfbn/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidden
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
