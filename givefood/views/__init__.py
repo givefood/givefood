0185cce4b5879508ce481ff086f730a549274bcc
Jason Cartwright <jason@givefood.org.uk>
2020-12-09 19:14:12 +0000

    move around public views

--- givefood/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from public import public_index, public_article, public_annual_report, public_gen_annual_report, public_product_image, distill_webhook, public_api, precacher, public_sitemap, public_reg_foodbank, public_privacy
