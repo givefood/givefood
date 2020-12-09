3a46c0fee4245587b2e06e46b15adbfa74cc0a8d
Jason Cartwright <jason@givefood.org.uk>
2020-12-09 18:22:16 +0000

    remove old api1 views

--- givefood/views/__init__.py
+++ givefood/views/__init__.py
@@ -1,3 +1,1 @@
 from public import public_index, public_article, public_annual_report, public_gen_annual_report, public_product_image, distill_webhook, public_api, precacher, public_sitemap, public_reg_foodbank, public_privacy
-
-from api import api_foodbanks, api_foodbank_search, api_foodbank, api_foodbank_key, api_needs, api_need
