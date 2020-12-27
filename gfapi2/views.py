fcc8693a46ed55a267549df329c3a3d1d30a30c4
Jason Cartwright <jason@givefood.org.uk>
2020-12-27 18:19:41 +0000

    add caching to more api2 responses

--- gfapi2/views.py
+++ gfapi2/views.py
@@ -13,6 +13,7 @@ from givefood.func import get_all_foodbanks, find_foodbanks, geocode, find_locat
 DEFAULT_FORMAT = "json"
 
 
+@cache_page(60*10)
 def index(request):
         
     template_vars = {}
@@ -20,6 +21,7 @@ def index(request):
     return render_to_response("index.html", template_vars, context_instance=RequestContext(request))
 
 
+@cache_page(60*10)
 def docs(request):
 
     api_formats = ["JSON","XML","YAML"]
@@ -61,6 +63,7 @@ def docs(request):
     return render_to_response("docs.html", template_vars, context_instance=RequestContext(request))
 
 
+@cache_page(60*20)
 def foodbanks(request):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -140,6 +143,7 @@ def foodbanks(request):
     return ApiResponse(response_list, "foodbanks", format)
 
 
+@cache_page(60*20)
 def foodbank(request, slug):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -237,6 +241,7 @@ def foodbank(request, slug):
     return ApiResponse(response_dict, "foodbank", format)
 
 
+@cache_page(60*20)
 def foodbank_search(request):
     
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -315,7 +320,7 @@ def foodbank_search(request):
     return ApiResponse(response_list, "foodbanks", format)
 
 
-@cache_page(60*10)
+@cache_page(60*20)
 def location_search(request):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -390,6 +395,7 @@ def location_search(request):
     return ApiResponse(response_list, "locations", format)
 
 
+@cache_page(60*5)
 def needs(request):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -418,6 +424,7 @@ def needs(request):
     return ApiResponse(response_list, "needs", format)
 
 
+@cache_page(60*10)
 def need(request, id):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
@@ -441,6 +448,7 @@ def need(request, id):
     return ApiResponse(response_dict, "need", format)
 
 
+@cache_page(60*20)
 def constituency(request, slug):
 
     format = request.GET.get("format", DEFAULT_FORMAT)
