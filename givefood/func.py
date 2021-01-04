6fe41ae2a0eabaeb90fb758c2d250287a42e9da7
Jason Cartwright <jason@givefood.org.uk>
2021-01-04 14:51:48 +0000

    handle unicode in geocoder closes #186

--- givefood/func.py
+++ givefood/func.py
@@ -68,7 +68,7 @@ def geocode(address):
 
     gmap_geocode_key = get_cred("gmap_geocode_key")
 
-    address_api_url = "https://maps.googleapis.com/maps/api/geocode/json?key=%s&address=%s" % (gmap_geocode_key, urllib.quote(address))
+    address_api_url = "https://maps.googleapis.com/maps/api/geocode/json?key=%s&address=%s" % (gmap_geocode_key, urllib.quote(address.encode('utf8')))
     address_api_result = urlfetch.fetch(address_api_url)
     if address_api_result.status_code == 200:
         try:
