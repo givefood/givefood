759636d2f0bba5b624262b507173d40be707e11f
Jason Cartwright <jason@givefood.org.uk>
2021-04-22 17:28:33 +0100

    improved dash_tt_most_requested_items
    day picker, keyword removal, foodbank and item counts

--- gfdash/views.py
+++ gfdash/views.py
@@ -1,5 +1,10 @@
 import logging
+
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+
 from givefood.models import Foodbank
 from givefood.func import group_list
 
@@ -22,30 +27,56 @@ def dash_tt_old_data(request):
 
 def dash_tt_most_requested_items(request):
 
-    # Empty items list we'll use
-    items = []
+    # Handle allowed day parameters
+    default_days = 30
+    allowed_days = [30, 60, 90, 120, 365]
+    days = int(request.GET.get("days", default_days))
+    if days not in allowed_days:
+        return HttpResponseForbidden()
+    day_threshold = datetime.now() - timedelta(days=days)
 
-    # Find most recent 250 Trussell Trust food banks by the date their last requested
-    recent_foodbanks = Foodbank.objects.filter(network = "Trussell Trust").order_by("-last_need")[:250]
+    # Empty vars we'll use
+    items = []
+    number_foodbanks = 0
+    number_items = 0
+
+    # Find the food banks that are in the Trussell Trust and have updated their needs within the day threshold
+    recent_foodbanks = Foodbank.objects.filter(network = "Trussell Trust", last_need__gt = day_threshold).order_by("-last_need")
+
+    # Keywords we use in need text that we'll exclude
+    invalid_text = ["Nothing", "Unknown"]
 
     # Loop the foodbanks
     for recent_foodbank in recent_foodbanks:
 
-        # Put the items of the last request from the food bank into the list
-        items.extend(recent_foodbank.latest_need().change_text.splitlines())
+        # Find need text
+        need_text = recent_foodbank.latest_need().change_text
 
-        # Keep tab on the last food bank we've found to allow us to show when the oldest data is from
-        oldest_foodbank = recent_foodbank
+        # Don't count the need if it's a keyword
+        if not need_text in invalid_text:
+
+            # Make list of items in this need
+            need_items = need_text.splitlines()
+
+            # Put the items of the last request from the food bank into the list
+            items.extend(need_items)
+
+        # Increment the food bank count
+        number_foodbanks += 1
 
     # Group the items by the item text
     items_freq = group_list(items)
+    number_items = len(items_freq)
 
     # Sort the items by their frequency count
     items_sorted = sorted(items_freq, reverse=True, key=lambda x: x[1])
 
     template_vars = {
-        "items_sorted":items_sorted,
-        "oldest_foodbank":oldest_foodbank,
+        "items_sorted": items_sorted,
+        "allowed_days": allowed_days,
+        "days": days,
+        "number_foodbanks": number_foodbanks,
+        "number_items": number_items,
     }
 
     return render(request, "dash_tt_most_requested_items.html", template_vars)
