123443d7aaaf01b70184e828f2b4c5652320a78e
Jason Cartwright <jason@givefood.org.uk>
2021-04-22 12:56:03 +0100

    comments on dash_tt_most_requested_items to show methodology

--- gfdash/views.py
+++ gfdash/views.py
@@ -22,15 +22,25 @@ def dash_tt_old_data(request):
 
 def dash_tt_most_requested_items(request):
 
+    # Empty items list we'll use
     items = []
 
+    # Find most recent 250 Trussell Trust food banks by the date their last requested
     recent_foodbanks = Foodbank.objects.filter(network = "Trussell Trust").order_by("-last_need")[:250]
 
+    # Loop the foodbanks
     for recent_foodbank in recent_foodbanks:
+
+        # Put the items of the last request from the food bank into the list
         items.extend(recent_foodbank.latest_need().change_text.splitlines())
+
+        # Keep tab on the last food bank we've found to allow us to show when the oldest data is from
         oldest_foodbank = recent_foodbank
 
+    # Group the items by the item text
     items_freq = group_list(items)
+
+    # Sort the items by their frequency count
     items_sorted = sorted(items_freq, reverse=True, key=lambda x: x[1])
 
     template_vars = {
