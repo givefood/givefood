516f08646c88bf13d42e94d6b652bcc4ea8d1de7
Jason Cartwright <jason@givefood.org.uk>
2021-05-04 22:37:17 +0100

    closes #210

--- gfwfbn/forms.py
+++ gfwfbn/forms.py
@@ -28,4 +28,11 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = Foodbank
-        fields = ("network","charity_number","facebook_page","twitter_handle","bankuet_slug","contact_email","phone_number","secondary_phone_number", "url", "shopping_list_url")
+        fields = ("network","charity_number","facebook_page","twitter_handle","contact_email","phone_number","secondary_phone_number", "url", "shopping_list_url")
+        help_texts = {
+            "charity_number": "Charity Commission for England and Wales, Office of the Scottish Charity Regulator, or Charity Commission for Northern Ireland charity number",
+            "facebook_page": "e.g. the 'GiveFoodOrgUK' part of facebook.com/GiveFoodOrgUK",
+            "twitter_handle": "e.g. the 'GiveFood_org_uk' part of twitter.com/GiveFood_org_uk",
+            "url": "Your web address",
+            "shopping_list_url": "Web address where we can see a list of the items you are requesting",
+        }
