ad8bed44aeb2bdf2f1023a80da75f95d8f9abb14
Jason Cartwright <jason@givefood.org.uk>
2021-06-12 08:59:51 +0100

    add offline app urls to SECURE_REDIRECT_EXEMPT

--- givefood/settings_live.py
+++ givefood/settings_live.py
@@ -16,7 +16,7 @@ SECURE_REDIRECT_EXEMPT = [
     # App Engine doesn't use HTTPS internally, so the /_ah/.* URLs need to be exempt.
     # Django compares these to request.path.lstrip("/"), hence the lack of preceding /
     r"^_ah/",
-    r"^precacher/",
+    r"^offline/",
 ]
 
 DEBUG = False
