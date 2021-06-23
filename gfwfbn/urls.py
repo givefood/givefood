88e1c130a28415bce39b12610269e55b7bfee58c
Jason Cartwright <jason@givefood.org.uk>
2021-06-23 11:00:09 +0100

    remove need clicker closes #221

--- gfwfbn/urls.py
+++ gfwfbn/urls.py
@@ -6,7 +6,6 @@ urlpatterns = (
     url(r'^tt-old-data/$', RedirectView.as_view(url='/dashboard/trusselltrust/old-data/')),
 
     url(r'^$', public_what_food_banks_need, name="public_what_food_banks_need"),
-    url(r'^click/(?P<slug>[-\w]+)/$', public_what_food_banks_need_click, name="public_what_food_banks_need_click"),
 
     url(r'^updates/(?P<action>subscribe|confirm|unsubscribe)/$', public_what_food_banks_need_updates, name="public_what_food_banks_need_updates"),
 
