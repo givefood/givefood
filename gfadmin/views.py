7526dc40468cb1caa92d59624502a63140f3c4e9
Jason Cartwright <jason@givefood.org.uk>
2021-06-30 18:39:12 +0100

    display order country in admin & on the csv

--- gfadmin/views.py
+++ gfadmin/views.py
@@ -168,9 +168,9 @@ def orders_csv(request):
     output = []
     response = HttpResponse (content_type='text/csv')
     writer = csv.writer(response)
-    writer.writerow(['id', 'created', 'delivery', 'delivery_provider', 'foodbank', 'weight', 'calories', 'items', 'cost'])
+    writer.writerow(['id', 'created', 'delivery', 'delivery_provider', 'foodbank', 'country', 'weight', 'calories', 'items', 'cost'])
     for order in orders:
-        output.append([order.order_id, order.created, order.delivery_datetime, order.delivery_provider, order.foodbank_name, order.weight, order.calories, order.no_items, order.cost])
+        output.append([order.order_id, order.created, order.delivery_datetime, order.delivery_provider, order.foodbank_name, order.country, order.weight, order.calories, order.no_items, order.cost])
     writer.writerows(output)
     return response
 
