dca9210b9285f0d5e98f5087efab99839424ea6b
Jason Cartwright <jason@givefood.org.uk>
2021-11-11 19:00:04 +0000

    dundee food bank redirect

--- givefood/urls.py
+++ givefood/urls.py
@@ -39,6 +39,7 @@ urlpatterns = (
     # Old Food Bank Redirects
     # https://github.com/givefood/givefood/issues/187
     url(r'^needs/at/angus/$', RedirectView.as_view(url='/needs/at/dundee-angus/')),
+    url(r'^needs/at/dundee/$', RedirectView.as_view(url='/needs/at/dundee-angus/')),
     url(r'^needs/at/lifeshare/$', RedirectView.as_view(url='/needs/at/lifeshare-manchester/')),
 
     # Apps
