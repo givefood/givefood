3830b5ca6594cf3e3e6382b324fb5899325483bb
Jason Cartwright <mail@jasoncartwright.com>
2023-01-30 10:04:32 +0000

    admin search

--- gfadmin/views.py
+++ gfadmin/views.py
@@ -13,7 +13,7 @@ from django.utils.encoding import smart_str
 from django.core.cache import cache
 
 from givefood.const.general import PACKAGING_WEIGHT_PC
-from givefood.func import get_all_foodbanks, get_all_locations, get_cred, post_to_facebook, post_to_twitter, post_to_subscriber, send_email
+from givefood.func import get_all_foodbanks, get_all_locations, get_cred, post_to_facebook, post_to_twitter, post_to_subscriber, send_email, get_all_constituencies
 from givefood.models import Foodbank, FoodbankGroup, Order, OrderGroup, OrderLine, OrderItem, FoodbankChange, FoodbankLocation, ApiFoodbankSearch, ParliamentaryConstituency, GfCredential, FoodbankSubscriber, FoodbankGroup
 from givefood.forms import FoodbankForm, OrderForm, NeedForm, FoodbankPoliticsForm, FoodbankLocationForm, FoodbankLocationPoliticsForm, OrderGroupForm, ParliamentaryConstituencyForm, OrderItemForm, GfCredentialForm, FoodbankGroupForm
 
@@ -74,9 +74,34 @@ def searches_csv(request):
 def search_results(request):
 
     query = request.GET.get("q")
-    foodbank = get_object_or_404(Foodbank, name=query)
+    all_foodbanks = get_all_foodbanks()
+    all_locations = get_all_locations()
+    all_constituencies = get_all_constituencies()
 
-    return redirect("admin:foodbank", slug = foodbank.slug)
+    foodbanks = []
+    locations = []
+    constituencies = []
+
+    for foodbank in all_foodbanks:
+        if query.lower() in foodbank.name.lower():
+            foodbanks.append(foodbank)
+    
+    for location in all_locations:
+        if query.lower() in location.name.lower():
+            locations.append(location)
+
+    for constituency in all_constituencies:
+        if query.lower() in constituency.name.lower():
+            constituencies.append(constituency)
+
+    template_vars = {
+        "query":query,
+        "foodbanks":foodbanks,
+        "locations":locations,
+        "constituencies":constituencies,
+        "section":"search",
+    }
+    return render(request, "admin/search.html", template_vars)
 
 
 def foodbanks(request):
