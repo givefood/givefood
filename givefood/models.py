2fb867b6ec6a8280e8e7dfc84159388e787e9a15
Jason Cartwright <mail@jasoncartwright.com>
2023-03-23 12:59:30 +0000

    address_is_administrative verbose name

--- givefood/models.py
+++ givefood/models.py
@@ -58,7 +58,7 @@ class Foodbank(models.Model):
     url = models.URLField(max_length=200, verbose_name="URL")
     shopping_list_url = models.URLField(max_length=200, verbose_name="Shopping list URL")
     rss_url = models.URLField(max_length=200, verbose_name="RSS feed URL", null=True, blank=True)
-    address_is_administrative = models.BooleanField(default=False)
+    address_is_administrative = models.BooleanField(default=False, verbose_name="Is the main address just used for administrative purposes?")
     is_closed = models.BooleanField(default=False)
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
