7fc2d4f4f3134196ea5a02935ab7b292c783da5d
Jason Cartwright <mail@jasoncartwright.com>
2023-04-30 14:26:40 +0100

    instance_id and version in html

--- givefood/context_processors.py
+++ givefood/context_processors.py
@@ -1,7 +1,11 @@
+import os
+
 def context(request):
 
     context = {
-        'CANONICAL_PATH': "https://www.givefood.org.uk%s" % (request.path),
+        'canonical_path': "https://www.givefood.org.uk%s" % (request.path),
+        'instance_id': os.environ.get('GAE_INSTANCE'),
+        'version': os.environ.get('GAE_VERSION'),
     }
 
     return context
