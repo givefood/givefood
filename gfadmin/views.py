bd3bf81c7dfd8e67cfd18e939e2f1d35dea3dc57
Jason Cartwright <mail@jasoncartwright.com>
2023-09-07 08:05:39 +0100

    fix broken admin need page

--- gfadmin/views.py
+++ gfadmin/views.py
@@ -474,11 +474,9 @@ def fblocation_delete(request, slug, loc_slug):
 def need(request, id):
 
     need = get_object_or_404(FoodbankChange, need_id = id)
-    number_subscribers = need.foodbank.number_subscribers()
     
     template_vars = {
         "need":need,
-        "number_subscribers":number_subscribers,
     }
     return render(request, "admin/need.html", template_vars)
 
