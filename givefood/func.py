0cf1888983f8df56a1b9009d3362b1d0260e1ba6
Jason Cartwright <mail@jasoncartwright.com>
2023-10-26 10:04:23 +0300

    limit decaching to 30 urls

--- givefood/func.py
+++ givefood/func.py
@@ -75,6 +75,10 @@ def get_all_constituencies():
 
 
 def decache(urls):
+    
+    # We can only uncache 30 URLs at a time
+    urls = urls[:30]
+    
     url_params = "&url=".join(urls)
     remote_cache_purge_url = "http://www.givefood.org.uk/purgecache/?url=%s" % (url_params)
     request = requests.get(remote_cache_purge_url)
