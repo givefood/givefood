77b7e3d4da852e702230f7ff2c6a738cddb95eed
Jason Cartwright <mail@jasoncartwright.com>
2024-01-19 11:33:02 +0000

    better csi naming

--- givefood/static/js/csi.js
+++ givefood/static/js/csi.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.querySelectorAll("*[data-csi]").forEach((element) => {
-        url = element.getAttribute("data-csi");
+    document.querySelectorAll("*[data-include]").forEach((element) => {
+        url = element.getAttribute("data-include");
         fetch(url).then((response) => {
             response.text().then((text) => {
                 element.innerHTML = text;
