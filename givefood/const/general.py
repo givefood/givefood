af95b479ed44bd6aa292ccd7d5893f533a881911
Jason Cartwright <mail@jasoncartwright.com>
2024-02-24 20:45:14 +0000

    more DONT_APPEND_FOOD_BANK

--- givefood/const/general.py
+++ givefood/const/general.py
@@ -130,6 +130,8 @@ DONT_APPEND_FOOD_BANK = [
     "Staffordshire Food and Furniture Bank",
     "The Shack Food Project",
     "Family Food Bank",
+    "Adat Yeshua Foodbank",
+    "New Hope Food Bank",
 ]
 
 POSTCODE_REGEX = "^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$"
