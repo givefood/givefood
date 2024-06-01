3503132c0957afec717c82e3a47fca48a5297ec5
Jason Cartwright <mail@jasoncartwright.com>
2024-06-01 12:44:53 +0100

    force postcodes.io decache

--- givefood/func.py
+++ givefood/func.py
@@ -838,7 +838,7 @@ def distance_meters(lat1, lon1, lat2, lon2):
 
 
 def admin_regions_from_postcode(postcode):
-    pc_api_url = "https://api.postcodes.io/postcodes/%s" % (urllib.parse.quote(postcode))
+    pc_api_url = "https://api.postcodes.io/postcodes/%s?decache=true" % (urllib.parse.quote(postcode))
     request = requests.get(pc_api_url)
     if request.status_code == 200:
         pc_api_json = request.json()
