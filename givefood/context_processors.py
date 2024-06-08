bd91af9820d9396dbfc48d54c6135ae81791c876
Jason Cartwright <mail@jasoncartwright.com>
2024-06-08 15:55:57 +0100

    app_name in context

--- givefood/context_processors.py
+++ givefood/context_processors.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+from django.urls import resolve
 
 from givefood.const.general import ENABLE_WRITE
 
@@ -9,6 +10,7 @@ def context(request):
         'instance_id': os.environ.get('GAE_INSTANCE'),
         'version': os.environ.get('GAE_VERSION'),
         'enable_write':ENABLE_WRITE,
+        'app_name': sys.modules[resolve(request.path_info).func.__module__].__package__,
     }
 
     return context
