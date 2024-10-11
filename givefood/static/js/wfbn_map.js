02b77414624e4a36043fb67a965a2f84891742f9
Jason Cartwright <mail@jasoncartwright.com>
2024-10-11 10:58:09 +0100

    simplify js

--- givefood/static/js/wfbn_map.js
+++ givefood/static/js/wfbn_map.js
@@ -114,21 +114,3 @@ function init_map() {
 
 var map
 google.maps.event.addDomListener(window, 'load', init_map);
-
-function slugify(str) {
-  str = str.replace(/^\s+|\s+$/g, ''); // trim
-  str = str.toLowerCase();
-
-  // remove accents, swap ñ for n, etc
-  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  var to   = "aaaaeeeeiiiioooouuuunc------";
-  for (var i=0, l=from.length ; i<l ; i++) {
-      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-  }
-
-  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-      .replace(/\s+/g, '-') // collapse whitespace and replace by -
-      .replace(/-+/g, '-'); // collapse dashes
-
-  return str;
-}
