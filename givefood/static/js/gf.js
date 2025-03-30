49d5ef5d4885e9d7ac64d2cc86d12000e0cd31f8
Jason Cartwright <mail@jasoncartwright.com>
2025-03-30 14:32:04 +0100

    only preload dropdown links once

--- givefood/static/js/gf.js
+++ givefood/static/js/gf.js
@@ -1,5 +1,7 @@
 document.querySelector(".dropdown-trigger button").addEventListener("mouseenter", function() {
     document.querySelectorAll(".dropdown-content a").forEach(function(el) {
-        document.querySelector("head").insertAdjacentHTML('beforeend','<link rel="prefetch" href="' + el.href + '">');
+        if (document.querySelector("link[rel='prefetch'][href='" + el.href + "']") == null) {
+            document.querySelector("head").insertAdjacentHTML('beforeend','<link rel="prefetch" href="' + el.href + '">');
+        }
     });
 });
