56c1a9b91480007afafa251cec8460eb64183f2b
Jason Cartwright <mail@jasoncartwright.com>
2025-04-24 20:05:13 +0100

    debug false

--- givefood/settings/default.py
+++ givefood/settings/default.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
