87051a761c8769b72c8c1c61aed50c7ec3f27986
Jason Cartwright <mail@jasoncartwright.com>
2025-05-06 22:07:10 +0100

    slightly longer version

--- givefood/context_processors.py
+++ givefood/context_processors.py
@@ -16,7 +16,7 @@ def context(request):
     canonical_path = "%s%s" % (SITE_DOMAIN, translated_path)
     querystring = request.META['QUERY_STRING']
     instance_id = os.environ.get('COOLIFY_CONTAINER_NAME')
-    version = os.environ.get('SOURCE_COMMIT')[:4]
+    version = os.environ.get('SOURCE_COMMIT')[:7]
     facebook_locale = FACEBOOK_LOCALES.get(language_code, "en_GB")
 
     page_translatable = "/cy/" == translate_url(path, "cy")[:4]
