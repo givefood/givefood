88db836c5f5ce14e868b2cc7280e6f27acf1408e
Jason Cartwright <mail@jasoncartwright.com>
2025-06-05 10:33:34 +0100

    handle no langswitcher

--- givefood/static/js/gf.js
+++ givefood/static/js/gf.js
@@ -1,7 +1,9 @@
-document.querySelector(".dropdown-trigger button").addEventListener("mouseenter", function() {
-    document.querySelectorAll(".dropdown-content a").forEach(function(el) {
-        if (document.querySelector("link[rel='prefetch'][href='" + el.href + "']") == null) {
-            document.querySelector("head").insertAdjacentHTML('beforeend','<link rel="prefetch" href="' + el.href + '">');
-        }
+if (document.querySelector(".langswitcher")) {
+        document.querySelector(".dropdown-trigger button").addEventListener("mouseenter", function() {
+            document.querySelectorAll(".dropdown-content a").forEach(function(el) {
+            if (document.querySelector("link[rel='prefetch'][href='" + el.href + "']") == null) {
+                document.querySelector("head").insertAdjacentHTML('beforeend','<link rel="prefetch" href="' + el.href + '">');
+            }
+        });
     });
-});
+}
