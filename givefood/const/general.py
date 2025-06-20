d3ad62eba6fbb2aef6787c2e6aad0949fbc1f43a
Jason Cartwright <mail@jasoncartwright.com>
2025-06-20 16:10:34 +0100

    DONT_APPEND_FOOD_BANK update

--- givefood/const/general.py
+++ givefood/const/general.py
@@ -204,6 +204,7 @@ DONT_APPEND_FOOD_BANK = [
     "Adat Yeshua Foodbank",
     "New Hope Food Bank",
     "Felix Multibank",
+    "Soar Valley Community Food Project",
 ]
 
 POSTCODE_REGEX = "^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$"
