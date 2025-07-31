bb3242eee72f8fd8ad4bd21ce088e98d5af0e23b
Jason Cartwright <mail@jasoncartwright.com>
2025-07-31 13:19:09 +0100

    dp and loc urls

--- givefood/models.py
+++ givefood/models.py
@@ -116,6 +116,8 @@ class Foodbank(models.Model):
     url = models.URLField(max_length=200, verbose_name="URL")
     shopping_list_url = models.URLField(max_length=200, verbose_name="Shopping list URL")
     rss_url = models.URLField(max_length=200, verbose_name="RSS feed URL", null=True, blank=True)
+    donation_points_url = models.URLField(max_length=200, verbose_name="Donation points URL", null=True, blank=True)
+    locations_url = models.URLField(max_length=200, verbose_name="Locations URL", null=True, blank=True)
 
     # Booleans
     address_is_administrative = models.BooleanField(default=False, verbose_name="Is the main address just used for administrative purposes?")
