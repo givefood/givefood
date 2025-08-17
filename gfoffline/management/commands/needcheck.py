b583b0b61a2b7044f6ed833855444eb1faf89e75
Jason Cartwright <mail@jasoncartwright.com>
2025-08-17 11:00:48 +0100

    needcheck mgmt cmd

---
+++ gfoffline/management/commands/needcheck.py
@@ -0,0 +1,22 @@
+import time
+from django.core.management.base import BaseCommand
+
+from givefood.func import do_foodbank_need_check
+from givefood.models import Foodbank
+
+
+class Command(BaseCommand):
+
+    help = 'Runs a specific view function from the command line.'
+
+    def handle(self, *args, **options):
+
+        foodbanks = Foodbank.objects.exclude(is_closed = True).exclude(shopping_list_url__contains = "facebook.com").exclude(hours_between_need_check = 0).order_by("?")
+        
+        for foodbank in foodbanks:
+            self.stdout.write(f"Check {foodbank.name}")
+            start_time = time.perf_counter()
+            do_foodbank_need_check(foodbank)
+            end_time = time.perf_counter()
+            elapsed_time = end_time - start_time
+            self.stdout.write(f"Done  {foodbank.name} ({elapsed_time:.2f} seconds)")
