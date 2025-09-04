78b2c8c6c5210584bdcb65a35b9294d6b11f2d37
Jason Cartwright <mail@jasoncartwright.com>
2025-09-04 19:59:52 +0100

    updated user agent

--- givefood/func.py
+++ givefood/func.py
@@ -1199,7 +1199,7 @@ def do_foodbank_need_check(foodbank, crawl_set = None):
     crawl_item.save()
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
     }
 
     try:
