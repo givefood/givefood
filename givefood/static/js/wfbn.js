6dd7bafb0ffff12c63ddf6bdea088cabb7ea47e6
Jason Cartwright <mail@jasoncartwright.com>
2025-10-24 12:51:48 -0700

    Refactor and modularize map and location JS code
    Improves readability and maintainability by modularizing the map and location logic into distinct functions, renaming variables for clarity, and adding documentation comments. The code now separates concerns for map initialization, autocomplete, location button, and info window handling, making future updates easier.

--- givefood/static/js/wfbn.js
+++ givefood/static/js/wfbn.js
@@ -1,199 +1,303 @@
-const ip_geolocation_url = "/needs/getlocation/"
-const address_field = document.querySelector("#address_field");
-const lat_lng_field = document.querySelector("#lat_lng_field");
-const uml_btn = document.querySelector("#usemylocationbtn");
-const addressform = document.querySelector("#addressform");
-const map_element = document.querySelector("#map");
+// Configuration
+const IP_GEOLOCATION_URL = "/needs/getlocation/";
+const GMAP_MAPID = "d149d3a77fb8625a";
 
+// DOM Elements
+const addressField = document.querySelector("#address_field");
+const latLngField = document.querySelector("#lat_lng_field");
+const useMyLocationBtn = document.querySelector("#usemylocationbtn");
+const addressForm = document.querySelector("#addressform");
+const mapElement = document.querySelector("#map");
+
+// Global Variables
+let map;
+let autocomplete;
+
+/**
+ * Initialize the page functionality
+ */
 function init() {
-    if (map_element) {
-      init_map()
+    if (mapElement) {
+        initMap();
     }
-    if (addressform) {
-      autocomplete = new google.maps.places.Autocomplete(address_field, {types:["geocode"]});
-      autocomplete.setComponentRestrictions({'country': ['gb', 'im', 'je', 'gg']});
-      autocomplete.addListener("place_changed", () => {
-        lat_lng_field.value = autocomplete.getPlace().geometry.location.lat() + "," + autocomplete.getPlace().geometry.location.lng();
-      })
-      if (uml_btn) {
-        uml_btn.addEventListener("click", function(event){
-            event.preventDefault();
-            uml_btn.classList.add("working");
-            url = uml_btn.getAttribute("data-url")
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                  function(position){
-                      lat  = position.coords.latitude;
-                      lng = position.coords.longitude;
-                      address_field.value = "";
-                      window.location = url + "?lat_lng=" + lat + "," + lng;
-                  }
-                );
-            }
-        });
-      }
+
+    if (addressForm) {
+        initAddressAutocomplete();
+    }
+
+    if (useMyLocationBtn) {
+        initLocationButton();
     }
 }
 
-function move_map(lat, lng, zoom) {
-    if (typeof map !== 'undefined') {
-        map.panTo(new google.maps.LatLng(lat, lng));
-        map.setZoom(zoom);
+/**
+ * Initialize Google Places autocomplete
+ */
+function initAddressAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(addressField, {
+        types: ["geocode"]
+    });
+    
+    autocomplete.setComponentRestrictions({
+        country: ["gb", "im", "je", "gg"]
+    });
+    
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+        const location = place.geometry.location;
+        latLngField.value = `${location.lat()},${location.lng()}`;
+    });
+}
+
+/**
+ * Initialize "Use My Location" button
+ */
+function initLocationButton() {
+    useMyLocationBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        useMyLocationBtn.classList.add("working");
         
-        if (gf_map_config.location_marker == true) {
-            const marker = new google.maps.Marker({
+        const url = useMyLocationBtn.getAttribute("data-url");
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+                addressField.value = "";
+                window.location = `${url}?lat_lng=${lat},${lng}`;
+            });
+        }
+    });
+}
+
+/**
+ * Move map to specified location and add optional marker
+ * @param {number} lat - Latitude
+ * @param {number} lng - Longitude
+ * @param {number} zoom - Zoom level
+ */
+function move_map(lat, lng, zoom) {
+    if (typeof map === "undefined") {
+        return;
+    }
+
+    map.panTo(new google.maps.LatLng(lat, lng));
+    map.setZoom(zoom);
+
+    if (gf_map_config.location_marker === true) {
+        new google.maps.Marker({
             position: new google.maps.LatLng(lat, lng),
             map: map,
             icon: {
                 path: google.maps.SymbolPath.CIRCLE,
-                scale: 7, // radius in pixels (7px radius = 14px diameter)
-                fillColor: '#4385F4',
+                scale: 7,
+                fillColor: "#4385F4",
                 fillOpacity: 0.8,
-                strokeColor: '#fff',
+                strokeColor: "#fff",
                 strokeWeight: 2,
             },
-            });
-        }
+        });
     }
 }
 
+/**
+ * Convert string to URL-friendly slug
+ * @param {string} str - String to slugify
+ * @returns {string} Slugified string
+ */
 function slugify(str) {
-    str = str.replace(/^\s+|\s+$/g, ''); // trim
-    str = str.toLowerCase();
-  
-    // remove accents, swap ñ for n, etc
-    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-    var to   = "aaaaeeeeiiiioooouuuunc------";
-    for (var i=0, l=from.length ; i<l ; i++) {
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    str = str.replace(/^\s+|\s+$/g, "").toLowerCase();
+
+    const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    const to = "aaaaeeeeiiiioooouuuunc------";
+    
+    for (let i = 0; i < from.length; i++) {
+        str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
     }
-  
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-        .replace(/\s+/g, '-') // collapse whitespace and replace by -
-        .replace(/-+/g, '-'); // collapse dashes
-  
+
+    str = str
+        .replace(/[^a-z0-9 -]/g, "")
+        .replace(/\s+/g, "-")
+        .replace(/-+/g, "-");
+
     return str;
-  }
+}
 
-
-function init_map() {
-    var infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(55.4,-4),
+/**
+ * Initialize Google Map with food bank locations
+ */
+function initMap() {
+    const infowindow = new google.maps.InfoWindow();
+    
+    map = new google.maps.Map(mapElement, {
+        center: new google.maps.LatLng(55.4, -4),
         zoom: 6,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControl: false,
         fullscreenControl: false,
         streetViewControl: false,
-        mapId: 'd149d3a77fb8625a',
+        mapId: GMAP_MAPID,
     });
-  
-    var data = new google.maps.Data();
-    data.loadGeoJson(gf_map_config.geojson, null, function(){
-        if (typeof gf_map_config.lat == 'undefined') {
-            bounds = new google.maps.LatLngBounds();
-            data.forEach(function(feature) {
-                geo = feature.getGeometry();
-                geo.forEachLatLng(function(LatLng) {
-                    bounds.extend(LatLng);
-                });
-            });
-            google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) {
-                if (gf_map_config.max_zoom) {
-                    max_zoom = gf_map_config.max_zoom
-                } else {
-                    max_zoom = 15
-                }
-                if (map.getZoom() > max_zoom) {
-                    map.setZoom(max_zoom);
-                }
-            });
-            map.fitBounds(bounds,{left:50, right:50, bottom:50, top:50});
-            map.panToBounds(bounds);
+
+    const data = new google.maps.Data();
+    
+    data.loadGeoJson(gf_map_config.geojson, null, () => {
+        if (typeof gf_map_config.lat === "undefined") {
+            fitMapToBounds(data);
         }
-        if (document.querySelector("#legendtemplate")) {
-            legendtemplate = document.querySelector("#legendtemplate").content.cloneNode(true)
-            legend = legendtemplate.querySelector("#legend")
-            map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
-            legend.style.display = "block";
-        }
+        
+        addMapLegend();
     });
-    data.setStyle(function(feature) {
-        if (feature.getProperty("type") == "lb") {
-            return {
-                fillColor: '#f7a723',
-                fillOpacity: 0.2,
-                strokeColor: '#f7a723',
-                strokeWeight: 1,
-            }
-        }
-        if (feature.getProperty("type") == "f") {
-            marker_colour = "red"
-            marker_size = 34
-        } else if (feature.getProperty("type") == "l") {
-            marker_colour = "yellow"
-            marker_size = 28
-        } else if (feature.getProperty("type") == "d") {
-            marker_colour = "blue"
-            marker_size = 24
-        } else if (feature.getProperty("type") == "b") {
-            marker_colour = ""
-            marker_size = 0
-        }
-        return {
-            icon: {
-                url: "/static/img/mapmarkers/" + marker_colour + ".png",
-                scaledSize: new google.maps.Size(marker_size, marker_size),
-            },
-            strokeWeight: 1,
-            title: feature.getProperty("name")
-        };
-    });
-    data.addListener('click', (event) => {
-        feat = event.feature
-        if (feat.getProperty('type') != "b") {
-            type = feat.getProperty('type');
-            title = feat.getProperty('name');
-            url = feat.getProperty('url');
-            address = feat.getProperty('address');
-            html = "<div class='infowindow'>"
-            html += "<h3>" + title + "</h3>"
-            if (type != "f") {
-                if (type == "l") {
-                    html += "<p>Location for "
-                }
-                if (type == "d") {
-                    html += "<p>Donation point for "
-                }
-                if (type == "lb") {
-                    html += "<p>Service area for "
-                }
-                html += "<a href='/needs/at/" + slugify(feat.getProperty('foodbank')) + "/'>" + feat.getProperty('foodbank') + "</a> Food Bank.</p>"
-            }
-            if (address) {
-                html += "<address>" + address.replace(/(\r\n|\r|\n)/g, '<br>') + "</address>"
-            }
-            html += "<a href='" + url + "' class='button is-info is-small'>More Information</a></div>"
-            infowindow.setContent(html);
-            if (event.latLng) {
-                infowindow.setPosition(event.latLng);
-            }
-            infowindow.setOptions({
-                maxWidth: 250,
-                pixelOffset: new google.maps.Size(0,-28),
-            });
-            infowindow.open(map);
-        }
-    });
+
+    data.setStyle((feature) => getFeatureStyle(feature));
+    data.addListener("click", (event) => handleMarkerClick(event, infowindow));
     data.setMap(map);
-  
-    if (typeof gf_map_config.lat !== 'undefined') {
-        move_map(
-            gf_map_config.lat,
-            gf_map_config.lng,
-            gf_map_config.zoom,
-        )
+
+    if (typeof gf_map_config.lat !== "undefined") {
+        move_map(gf_map_config.lat, gf_map_config.lng, gf_map_config.zoom);
     }
-  }
-  
-  var map
+}
+
+/**
+ * Fit map to show all markers
+ * @param {google.maps.Data} data - Map data layer
+ */
+function fitMapToBounds(data) {
+    const bounds = new google.maps.LatLngBounds();
+    
+    data.forEach((feature) => {
+        const geometry = feature.getGeometry();
+        geometry.forEachLatLng((latLng) => {
+            bounds.extend(latLng);
+        });
+    });
+
+    google.maps.event.addListenerOnce(map, "bounds_changed", () => {
+        const maxZoom = gf_map_config.max_zoom || 15;
+        if (map.getZoom() > maxZoom) {
+            map.setZoom(maxZoom);
+        }
+    });
+
+    const padding = { left: 50, right: 50, bottom: 50, top: 50 };
+    map.fitBounds(bounds, padding);
+    map.panToBounds(bounds);
+}
+
+/**
+ * Add legend to map if template exists
+ */
+function addMapLegend() {
+    const legendTemplate = document.querySelector("#legendtemplate");
+    
+    if (legendTemplate) {
+        const legendClone = legendTemplate.content.cloneNode(true);
+        const legend = legendClone.querySelector("#legend");
+        map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
+        legend.style.display = "block";
+    }
+}
+
+/**
+ * Get style for map feature based on type
+ * @param {google.maps.Data.Feature} feature - Map feature
+ * @returns {object} Style configuration
+ */
+function getFeatureStyle(feature) {
+    const type = feature.getProperty("type");
+
+    if (type === "lb") {
+        return {
+            fillColor: "#f7a723",
+            fillOpacity: 0.2,
+            strokeColor: "#f7a723",
+            strokeWeight: 1,
+        };
+    }
+
+    const markerConfig = {
+        f: { colour: "red", size: 34 },
+        l: { colour: "yellow", size: 28 },
+        d: { colour: "blue", size: 24 },
+        b: { colour: "", size: 0 },
+    };
+
+    const config = markerConfig[type] || { colour: "", size: 0 };
+
+    return {
+        icon: {
+            url: `/static/img/mapmarkers/${config.colour}.png`,
+            scaledSize: new google.maps.Size(config.size, config.size),
+        },
+        strokeWeight: 1,
+        title: feature.getProperty("name"),
+    };
+}
+
+/**
+ * Handle click on map marker
+ * @param {object} event - Click event
+ * @param {google.maps.InfoWindow} infowindow - Info window instance
+ */
+function handleMarkerClick(event, infowindow) {
+    const feature = event.feature;
+    const type = feature.getProperty("type");
+
+    if (type === "b") {
+        return;
+    }
+
+    const html = buildInfoWindowContent(feature);
+    
+    infowindow.setContent(html);
+    
+    if (event.latLng) {
+        infowindow.setPosition(event.latLng);
+    }
+    
+    infowindow.setOptions({
+        maxWidth: 250,
+        pixelOffset: new google.maps.Size(0, -28),
+    });
+    
+    infowindow.open(map);
+}
+
+/**
+ * Build HTML content for info window
+ * @param {google.maps.Data.Feature} feature - Map feature
+ * @returns {string} HTML content
+ */
+function buildInfoWindowContent(feature) {
+    const type = feature.getProperty("type");
+    const title = feature.getProperty("name");
+    const url = feature.getProperty("url");
+    const address = feature.getProperty("address");
+    const foodbank = feature.getProperty("foodbank");
+
+    let html = "<div class='infowindow'>";
+    html += `<h3>${title}</h3>`;
+
+    if (type !== "f") {
+        const typeLabels = {
+            l: "Location for",
+            d: "Donation point for",
+            lb: "Service area for",
+        };
+        
+        const label = typeLabels[type] || "";
+        const foodbankSlug = slugify(foodbank);
+        
+        html += `<p>${label} <a href='/needs/at/${foodbankSlug}/'>${foodbank}</a> Food Bank.</p>`;
+    }
+
+    if (address) {
+        const formattedAddress = address.replace(/(\r\n|\r|\n)/g, "<br>");
+        html += `<address>${formattedAddress}</address>`;
+    }
+
+    html += `<a href='${url}' class='button is-info is-small'>More Information</a>`;
+    html += "</div>";
+
+    return html;
+}
