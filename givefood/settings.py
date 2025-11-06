d879ec7b255d4decdab3e809aeb7cc81a538ae81
Jason Cartwright <mail@jasoncartwright.com>
2025-11-06 12:37:14 +0000

    Set database connection max age to 600 seconds
    Added 'CONN_MAX_AGE': 600 to the default database settings to enable persistent connections and improve performance.

--- givefood/settings.py
+++ givefood/settings.py
@@ -135,6 +135,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASS"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": "5432",
+        'CONN_MAX_AGE': 600,
     }
 }
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField" 
